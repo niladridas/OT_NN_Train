62d1d906683db7dd373d5b1fcf91c5a5572123b8
NILADRI DAS <niladridas1989@tamu.edu>
2019-03-25 13:06:18 -0500

    readme.md edited

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# UKF
-UKF in Python for State, Parameter, and Dual Estimation 
+# Unscented Kalman Filter
+UKF in Python for **State, Parameter, and Dual Estimation**
+****
+This work is based on:
+**[The Unscented Kalman Filter for Nonlinear Estimation]( https://www.seas.harvard.edu/courses/cs281/papers/unscented.pdf)**
+****
+Contributors:
+1. Vaishnav
+2. Niladri Das
+3. Vedang Deshpande
+****
---
+++ timeprop.py
@@ -0,0 +1,16 @@
+# Takes as input 4 functions:
+# 1. State dynamics
+# 2. State measurement model
+# 3. Parameter dynamics
+# 4. Parameter measurement model
+#
+from scipy.integrate import ode
+
+# Ode integration
+r = ode(f, jac).set_integrator('zvode', method='bdf', with_jacobian=True)
+r.set_initial_value(y0, t0).set_f_params(2.0).set_jac_params(2.0)
+t1 = 10
+dt = 1
+while r.successful() and r.t < t1:
+    r.integrate(r.t+dt)
+    print("%g %g" % (r.t, r.y))

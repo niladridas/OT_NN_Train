1a77d39156e5d1f1dd9aee41ac15841c07196a7f
NILADRI DAS <niladridas1989@tamu.edu>
2019-04-18 20:46:07 -0500

    editing

--- ukfupdate.py
+++ ukfupdate.py
@@ -1,23 +1,23 @@
 import numpy as np
 from numpy.linalg import inv
 
-def ukfupdate(xSigmapts,ySigmapts,yObs):
+
+def ukfupdate(xsigmapts, ysigmapts, yobs, sigw):
     # Calculating the mean of xSigmapts
-    xM = xSigmapts.mean(1)
+    xm = xsigmapts.mean(1)
     # Calculating the mean of ySigmapts
-    yM = ySigmapts.mean(1)
+    ym = ysigmapts.mean(1)
     # Calculating Pxx
-    L = np.shape(xSigmapts)[1]
-    Ly = np.shape(ySigmapts)[1]
-    Pxx = np.zeros(L,L)
-    Pyy = np.zeros(L,L)
-    Pxy = np.zeros(L,L)
-    for i in range (0,2*L):
-        Pxx = Pxx + sigW.wc[i]*np.matmul(xSigmapts[:,i]-xM,(xSigmapts[:,i]-xM).transpose())
-        Pyy = Pyy + sigW.wc[i]*np.matmul(ySigmapts[:,i]-yM,(ySigmapts[:,i]-yM).transpose())
-        Pxy = Pxy + sigW.wc[i]*np.matmul(xSigmapts[:,i]-xM,(ySigmapts[:,i]-yM).transpose())
-    K = np.matmul(Pxy,inv(Pyy))
-    xMpost =  xM + np.matmul(K,(yObs-yM))
-    xCpost = Pxx - np.matmul(np.matmul(K,Pyy),K.transpose())
-    updatedata = {"xMpost": xMpost, "xCpost": xCpost}
+    l1 = np.shape(xsigmapts)[1]
+    pxx = np.zeros(l1, l1)
+    pyy = np.zeros(l1, l1)
+    pxy = np.zeros(l1, l1)
+    for i in range(0, 2*l1):
+        pxx = pxx + sigw.wc[i]*np.matmul(xsigmapts[:,i]-xm,(xsigmapts[:,i]-xm).transpose())
+        pyy = pyy + sigw.wc[i]*np.matmul(ysigmapts[:,i]-ym,(ysigmapts[:,i]-ym).transpose())
+        pxy = pxy + sigw.wc[i]*np.matmul(xsigmapts[:,i]-xm,(ysigmapts[:,i]-ym).transpose())
+    K = np.matmul(pxy,inv(pyy))
+    xmpost = xm + np.matmul(K, (yobs-ym))
+    xcpost = pxx - np.matmul(np.matmul(K, pyy), K.transpose())
+    updatedata = {"xmpost": xmpost, "xcpost": xcpost}
     return updatedata

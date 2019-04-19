b296be7e31a41ee10e416abb150f1aa48ede37fe
NILADRI DAS <niladridas1989@tamu.edu>
2019-04-19 16:13:04 -0500

    working on Dual UKF

---
+++ testdualUKF.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+
+def main():
+    """ Dual UKF.
+
+    This function test the dual UKF algorithm.
+    Two UKF algorithms are run simultaneously.
+    The first algorithm is for updating the weights.
+    These updates weights are used in the next UKF algorithm to update the states.
+    The updated states are then fed back to the first UKF algorithm.
+    """
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
+
+
+#TO-DO: Observation sequence
+# yobs = # An array with dimension no_of_y_states X no_of_observations
+
+# TO-DO: Initializing the weight filter
+
+# TO-DO: Initializing the signal filter
+

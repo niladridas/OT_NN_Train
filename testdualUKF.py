c19e385e8a1935d51ccb99712ab6dd38d6f335e5
NILADRI DAS <niladridas1989@tamu.edu>
2019-04-19 18:40:53 -0500

    all

--- testdualUKF.py
+++ testdualUKF.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 # yobs = # An array with dimension no_of_y_states X no_of_observations
 
 # TO-DO: Weight filter
-def weightpropdualukf(wsigmapts, sigw, mu):
+def weightpropdualukf(wsigmapts, sigw, mu, xest, xprocesscov, ymeascov):
   """ Weight update function for Dual UKF
 
   : params: mu : puu = mu*pww (from paper), for data length of 1000, mu = 1e-4
@@ -31,8 +31,10 @@ def weightpropdualukf(wsigmapts, sigw, mu):
   # W_next = w_previous + u
   # u: zero mean noise with cov Pu = mu*Pw
   # TO-DO: Pw: calculated from the wsigmapts (not clear from the paper)
-  wm = wsigmapts.mean(0)
   l1 = np.shape(wsigmapts)[0]
+  wm = np.zeros((l1, 1))
+  for i in range(0, 2 * l1):
+    wm = wm + sigw.wm[i]*wsigmapts[:, i]
   for i in range(0, 2 * l1):
     pww = pww + sigw.wc[i] * np.matmul(wsigmapts[:, i] - wm, (wsigmapts[:, i] - wm).transpose())
   puu = mu*pww
@@ -41,6 +43,7 @@ def weightpropdualukf(wsigmapts, sigw, mu):
   for i in range(0, 2 * l1):
     u = np.random.multivariate_normal(meanu, covu, (3, 3))
     wsigmapts[:, i] = wsigmapts[:, i] + u
+  for i in range(0, 2 * l1):
 
 
 def weightupdatedualukf():

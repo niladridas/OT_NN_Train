585ee76d054a1f2a5090785caded9a3202a41b3a
NILADRI DAS <niladridas1989@tamu.edu>
2019-05-09 15:46:23 -0500

    all

--- MATLAB/utils/weightbiasup.m
+++ MATLAB/utils/weightbiasup.m
@@ -3,21 +3,21 @@ function [Wnext,Bnext] = weightbiasup(NN,x,y,eta,CdW)
         CdW{i} = zeros(size(NN.W{i},1),size(NN.W{i},2));
     end
     for i = 1:size(x,1) % Number of individual samples
-    % Forward propagation
-    [A,Z] = forwardprop(NN,x(i,:));
+        % Forward propagation
+        [A,Z] = forwardprop(NN,x(i,:));
 
-    % Error signal in the final layer
-    E = y(i,:)-A{end};
+        % Error signal in the final layer
+        E = y(i,:)-A{end};
 
-    % Backward propagation
-    Delta = backprop(E,Z,NN);
+        % Backward propagation
+        Delta = backprop(E,Z,NN);
 
-    % Derivative of the cost with respect to weights
-    tmpCdW = CderW(NN.W,Delta,A,x(i,:)') ;
+        % Derivative of the cost with respect to weights
+        tmpCdW = CderW(NN.W,Delta,A,x(i,:)') ;
 
-    for t = 1:length(NN.W)
-        CdW{t} = CdW{t}+tmpCdW{t}/(size(x,1));
-    end  
+        for t = 1:length(NN.W)
+            CdW{t} = CdW{t}+tmpCdW{t}/(size(x,1));
+        end  
     end
     % Weight update
     for i = 1:length(NN.W)

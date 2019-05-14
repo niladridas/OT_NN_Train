5341eb49d0395e3bc79862e47129d09fdc0ae351
vedang <vedang.28@gmail.com>
2019-05-14 14:10:06 -0500

    another algo for sigma pts

--- MATLAB/utils/getSigmaPts.m
+++ MATLAB/utils/getSigmaPts.m
@@ -11,17 +11,31 @@ function [xSigmaPts,Wm,Wc] = getSigmaPts(xMean,Px,a,k,b)
     L = length(xMean); % dim of x
     nPt = 2*L+1; % number of sigma pts
     lambda = a^2*(L+k)-L;
-    TEMP = sqrtm((L+lambda)*Px)'; % Mind the transpose
-    
+%     TEMP = sqrtm((L+lambda)*Px)'; % Mind the transpose
+%     
+%     % Sigma Pts
+%     xSigmaPts = zeros(L,nPt);
+%     xSigmaPts(:,1) = xMean;
+%     for i = 1:L
+%         xSigmaPts(:,i+1) = xMean + TEMP(:,i);
+%         xSigmaPts(:,i+1+L) = xMean - TEMP(:,i);
+%     end
+%     
+%     % Weights
+%     Wm = 0.5*ones(1,nPt)/(L+lambda); Wm(1,1) = lambda/(L+lambda);
+%     Wc = Wm; Wc(1,1) = lambda/(L+lambda)+(1-a^2+b);
+
     % Sigma Pts
+    W0 = 2*rand-1;
     xSigmaPts = zeros(L,nPt);
     xSigmaPts(:,1) = xMean;
+    TEMP = sqrtm((L/(1-W0))*Px)';
     for i = 1:L
         xSigmaPts(:,i+1) = xMean + TEMP(:,i);
         xSigmaPts(:,i+1+L) = xMean - TEMP(:,i);
     end
-    
-    % Weights
-    Wm = 0.5*ones(1,nPt)/(L+lambda); Wm(1,1) = lambda/(L+lambda);
-    Wc = Wm; Wc(1,1) = lambda/(L+lambda)+(1-a^2+b);
+    Wm = (1-W0)*ones(1,nPt)/(2*L); Wm(1,1) = W0;
+    Wc=Wm;
+
+
 end

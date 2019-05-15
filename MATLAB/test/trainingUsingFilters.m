86e3aca123c5dbff9b753de828bfe52a51859837
vedang <vedang.28@gmail.com>
2019-05-15 12:38:16 -0500

    training using epochs
    EKF/UKF give flat lines

--- MATLAB/test/trainingUsingFilters.m
+++ MATLAB/test/trainingUsingFilters.m
@@ -6,6 +6,8 @@ clc; clear; close all;
 %% Initialize
 load('data/trained_NN_complete_data.mat')
 NN_EKF = NNconstruct(ni,Ln); % We will train this NN using EKF
+% w_true = nn2param(NN); 
+% NN_EKF = param2nn(NN_EKF,w_true);
 NN_UKF = NN_EKF;
 x0 = nn2param(NN_EKF); % Initial state
 nx  = length(x0); % Number of states
@@ -13,43 +15,53 @@ ny = Ln(end); % Number of measurements = no. of o/p of NN
 Inx  = eye(nx);
 Ast = Inx; % State transition matrix is identity
 
-var_meas = 0.1; % variance of measurement noise; sigma^2
-var_proc = 0.01; % variance of process noise
-var_initState = 1; % initial state covariance
+var_meas = 0.01; % variance of measurement noise; sigma^2
+var_proc = 0.05; % variance of process noise
+var_initState = 1e6; % initial state covariance
 
 P0 = var_initState*Inx; % Initial state covariance matrix
 Q = var_proc*Inx; % Process noise covariance matrix
 R = var_meas*eye(ny); % Measurements noise covariance matrix
-% y1 = y1(1:100);
+y1 = y1(1:100);
 yMeas = y1 + normrnd(0,var_meas,[length(y1),1]); % Synthetic Noisy measurements
 kEnd = length(yMeas);
+maxEpoch = 1;
 
 %% EKF Code
 x_prev = x0; % estimate of x(k-1)
 P_prev = P0; 
-yEKF=zeros(kEnd,1);
-for k = 1:kEnd
-    clc; 
-    fprintf('EKF k = %d.\n',k);
-    
-    % EKF Propagation/Prediction
-    x_pr = Ast*x_prev; % x-(k) a priori state estimate
-    P_pr = Ast*P_prev*Ast' + Q; % P-(k) a priori state covariance matrix
-    
-    % EKF Update
-    NN_EKF = param2nn(NN_EKF,x_pr); % Update parameters of the NN
-    res = yMeas(k,1) - measModel(NN_EKF,iP(k,:)'); % Innovation/ Measurement residual
-    H = nnJacobian(NN_EKF,iP(k,:)'); % Jacobian of measurement model w.r.t states (NN Params in this case)
-    KK = P_pr*H'/(H*P_pr*H'+R); % Kalman Gain
-    x_pst = x_pr + KK*res; % a posteriori state estimate
-    P_pst = (Inx-KK*H)*P_pr; % a posteriori state covariance matrix
-    
-    x_prev = x_pst; 
-    P_prev = P_pst; 
-    
-    % Evaluate o/p of NN using a posteriori parameters estimates
+yEKF=zeros(kEnd,maxEpoch);
+for iEp = 1:maxEpoch
+    fprintf('EKF: Epoch = %d.\n',iEp);
+    fprintf('norm = %d.\n',norm(x_prev-x0)); 
+    for k = 1:kEnd
+    %     clc; 
+    %     fprintf('EKF: Epoch = %d, k = %d.\n',iEp,k);
+
+        % EKF Propagation/Prediction
+        x_pr = Ast*x_prev; % x-(k) a priori state estimate
+        P_pr = Ast*P_prev*Ast' + Q; % P-(k) a priori state covariance matrix
+
+        % EKF Update
+        NN_EKF =  param2nn(NN_EKF,x_pr); % Update parameters of the NN
+        res = yMeas(k,1) - measModel(NN_EKF,iP(k,:)'); % Innovation/ Measurement residual
+        H = nnJacobian(NN_EKF,iP(k,:)'); % Jacobian of measurement model w.r.t states (NN Params in this case)
+        KK = P_pr*H'/(H*P_pr*H'+R); % Kalman Gain
+        x_pst = x_pr + KK*res; % a posteriori state estimate
+        P_pst = (Inx-KK*H)*P_pr; % a posteriori state covariance matrix
+
+        x_prev = x_pst; 
+        P_prev = P_pst; 
+    %     
+    %     NN_EKF = param2nn(NN_EKF,x_pst); % Update parameters of the NN
+    %     yEKF(k,iEp) = measModel(NN_EKF,iP(k,:)');
+    end
+    % Evaluate o/p of NN using a posteriori parameters estimates at the end
+    % of this epoch, for all input sets
     NN_EKF = param2nn(NN_EKF,x_pst); % Update parameters of the NN
-    yEKF(k,1) = measModel(NN_EKF,iP(k,:)');
+    for k = 1:kEnd
+        yEKF(k,iEp) = measModel(NN_EKF,iP(k,:)');
+    end
 end
 
 %% UKF Code 
@@ -62,8 +74,9 @@ PAug0 = diag([diag(P0);diag(Q);diag(R)]);
 
 xAug_prev = xAug0; 
 PAug_prev = PAug0; 
-yUKF=zeros(kEnd,1);
-for k = 1:kEnd
+yUKF=zeros(kEnd,maxEpoch);
+for iEp = 1:maxEpoch
+for k = 1:1
     clc; 
     fprintf('UKF k = %d.\n',k);
     
@@ -105,23 +118,28 @@ for k = 1:kEnd
                  zeros(nx,nx+ny)', diag([diag(Q);diag(R)])];
              
     % Evaluate o/p of NN using a posteriori parameters estimates
-    NN_UKF = param2nn(NN_UKF,x_pst); % Update parameters of the NN
-    yUKF(k,1) = measModel(NN_UKF,iP(k,:)');
+%     NN_UKF = param2nn(NN_UKF,x_pst); % Update parameters of the NN
+%     yUKF(k,iEp) = measModel(NN_UKF,iP(k,:)');
 end
-
+    % Evaluate o/p of NN using a posteriori parameters estimates
+    NN_UKF = param2nn(NN_UKF,x_pst); % Update parameters of the NN
+    for k = 1:kEnd
+        yUKF(k,iEp) = measModel(NN_UKF,iP(k,:)');
+    end
+end
 
 %% Plot
 figure(1); clf; hold on; box; grid;
 plot(y1,'r','LineWidth',1) % Clean output
-plot(yEKF,'b','LineWidth',1) % Estimated using EKF
-plot(yUKF,'g-.','LineWidth',1) % Estimated using UKF
+plot(yEKF,'b-','LineWidth',1) % Estimated using EKF
+plot(yUKF,'g--','LineWidth',1) % Estimated using UKF
 plot(yMeas,'k+','MarkerSize',3) % Noisy measurements
 legend('Clean','EKF','UKF','Noisy')
 set(gcf,'Position',[370,554,929,370])
-saveas(gcf,'plots/trainingUsingFilters.png')
+% saveas(gcf,'plots/trainingUsingFilters.png')
 
 figure(2); clf; hold on; box; grid;
 plot(abs(yEKF-y1)./abs(y1),'b','LineWidth',1) % Estimated using EKF
-plot(abs(yUKF-y1)./abs(y1),'g-.','LineWidth',1) % Estimated using UKF
+plot(abs(yUKF-y1)./abs(y1),'g--','LineWidth',1) % Estimated using UKF
 legend('EKF','UKF')
 set(gcf,'Position',[368,118,933,347])

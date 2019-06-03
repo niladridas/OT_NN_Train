9c2b0e96148cc9bdbb5ff94937461178c109165e
Niladri Das <niladridas95@gmail.com>
2019-06-02 19:36:11 -0500

    added OT 16 to 20

--- MATLAB/test/gatherOTFydata.m
+++ MATLAB/test/gatherOTFydata.m
@@ -3,13 +3,17 @@
 % Gathering the OT data
 clc; clear; close all;
 D1 = load('./data/OTF_ref_1_to_5.mat');
-D2 = load('./data/OTF_ref_11_to_15.mat');
+D2 = load('./data/OTF_ref_6_to_10.mat');
+D3 = load('./data/OTF_ref_11_to_15.mat');
+D4 = load('./data/OTF_ref_16_to_20.mat');
 % yOTF is the OT filter output data set
 % dim(yOTF) = 200x10x20 : 10 epochs and 20 repititions
 [samples,epochs,rep] = size(D1.yOTF);
 % We have 2 sets of data: total 10 repititions 
-yOTF = zeros(200,10,10);
+yOTF = zeros(200,10,20);
 yOTF(:,:,1:5) = D1.yOTF(:,:,1:5);
-yOTF(:,:,6:10) = D2.yOTF(:,:,11:15);
+yOTF(:,:,6:10) = D2.yOTF(:,:,6:10);
+yOTF(:,:,11:15) = D2.yOTF(:,:,11:15);
+yOTF(:,:,16:20) = D2.yOTF(:,:,16:20);
 save('./data/OT_1_5_11_15.mat','yOTF');
 
